025a98673585b8bc2e97afe91a916b8a19212b97
Oleksii Pustovit <91pustovit91@gmail.com>
2022-10-20 21:35:53 +0300

    creat all expenses block

--- src/createServer.js
+++ src/createServer.js
@@ -127,6 +127,141 @@ function createServer() {
     res.send(foundUser);
   });
 
+  const expenses = [];
+
+  app.get('/expenses', express.json(), (req, res) => {
+    const { userId, category, from, to } = req.query;
+
+    if (category) {
+      const foundExpenses = expenses.filter(
+        (expense) => expense.category === category
+      );
+
+      res.statusCode = 200;
+
+      res.send(foundExpenses);
+
+      return;
+    }
+
+    if (userId) {
+      const foundExpenses = expenses.filter(
+        (expense) => expense.userId === Number(userId)
+      );
+
+      res.statusCode = 200;
+
+      res.send(foundExpenses);
+
+      return;
+    }
+
+    if (from && to) {
+      const foundExpenses = expenses.filter(
+        (expense) => expense.spentAt >= from && expense.spentAt <= to
+      );
+
+      res.statusCode = 200;
+
+      res.send(foundExpenses);
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    res.send(expenses);
+  });
+
+  app.post('/expenses', express.json(), (req, res) => {
+    const { userId } = req.body;
+
+    const foundUser = users.find((user) => user.id === Number(userId));
+
+    if (!foundUser) {
+      res.statusCode = 400;
+
+      res.send();
+
+      return;
+    }
+
+    const newExpense = {
+      id: expenses.length + 1,
+      ...req.body,
+    };
+
+    expenses.push(newExpense);
+
+    res.statusCode = 201;
+
+    res.send(newExpense);
+  });
+
+  app.get('/expenses/:expenseId', (req, res) => {
+    const { expenseId } = req.params;
+
+    const foundExpense = expenses.find(
+      (expense) => expense.id === Number(expenseId)
+    );
+
+    if (!foundExpense) {
+      res.statusCode = 404;
+
+      res.send();
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    res.send(foundExpense);
+  });
+
+  app.delete('/expenses/:expenseId', (req, res) => {
+    const { expenseId } = req.params;
+
+    const filteredExpenses = expenses.filter(
+      (expense) => expense.id !== Number(expenseId)
+    );
+
+    if (filteredExpenses.length === expenses.length) {
+      res.statusCode = 404;
+
+      res.send();
+
+      return;
+    }
+
+    expenses.splice(0, expenses.length, ...filteredExpenses);
+
+    res.statusCode = 204;
+
+    res.send();
+  });
+
+  app.patch('/expenses/:expenseId', express.json(), (req, res) => {
+    const { expenseId } = req.params;
+
+    const foundExpense = expenses.find(
+      (expense) => expense.id === Number(expenseId)
+    );
+
+    if (!foundExpense) {
+      res.statusCode = 404;
+
+      res.send();
+
+      return;
+    }
+
+    Object.assign(foundExpense, req.body);
+
+    res.statusCode = 200;
+
+    res.send(foundExpense);
+  });
+
   return app;
 }
 
