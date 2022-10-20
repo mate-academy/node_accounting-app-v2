c20120be4bac055c32e3ed418d8084e27bb1fd36
Maks Sobko <tiserettt@gmail.com>
2022-10-20 17:06:39 +0300

    implemented get request for expenses & users

--- src/createServer.js
+++ src/createServer.js
@@ -41,6 +41,57 @@ function createServer() {
     res.send(newExpense);
   });
 
+  app.get('/expenses', express.json(), (req, res) => {
+    const {
+      userId,
+      category,
+      from,
+      to,
+    } = req.query;
+
+    const id = Number(userId);
+
+    if (typeof id !== 'number') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    if (!expenses.length) {
+      res.send([]);
+
+      return;
+    }
+
+    if (from && to) {
+      const userExpenses = expenses.filter(
+        expense => expense.spentAt > from && expense.spentAt < to
+      );
+
+      res.send(userExpenses);
+
+      return;
+    }
+
+    if (users.find(user => user.id === id)) {
+      let userExpenses = expenses.filter(expense => expense.userId === id);
+
+      if (category) {
+        userExpenses = userExpenses.filter(
+          expense => expense.category === category
+        );
+      }
+
+      res.send(userExpenses);
+
+      return;
+    }
+
+    res.send(expenses);
+  });
+
   app.post('/users', express.json(), (req, res) => {
     const { name } = req.body;
 
@@ -59,6 +110,20 @@ function createServer() {
 
     res.statusCode = 201;
     res.send(newUser);
+  });
+
+  app.get('/users', express.json(), (req, res) => {
+    if (!users) {
+      res.statusCode = 200;
+
+      res.send([]);
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    res.send(users);
   });
 
   return app;
