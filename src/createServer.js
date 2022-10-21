d9e41b42682f1510f3d0fcfda977562f4cb1376b
Andrew Zaitsev <alzay007@gmail.com>
2022-10-21 11:34:36 +0300

    basic version

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,242 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  let users = [];
+  let expenses = [];
+
+  app.use(cors());
+
+  app.get('/users', (req, res) => {
+    if (!users) {
+      res.statusCode = 200;
+
+      res.send([]);
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.send(users);
+  });
+
+  app.get('/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    const foundUser = users.find(user => user.id === +userId);
+
+    if (!foundUser) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    res.send(foundUser);
+  });
+
+  app.post('/users', express.json(), (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const newUser = {
+      id: Math.max(0, ...users.map(({ id }) => id + 1)),
+      name,
+    };
+
+    users.push(newUser);
+
+    res.statusCode = 201;
+    res.send(newUser);
+  });
+
+  app.delete('/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    const filteredUsers = users.filter(user => user.id !== +userId);
+
+    if (filteredUsers.length === users.length) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    users = filteredUsers;
+
+    res.sendStatus(204);
+  });
+
+  app.patch('/users/:userId', express.json(), (req, res) => {
+    const { userId } = req.params;
+    const { name } = req.body;
+
+    const foundUser = users.find(user => user.id === +userId);
+
+    if (!foundUser) {
+      res.statusCode = 404;
+
+      return;
+    }
+
+    if (typeof name !== 'string') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    Object.assign(foundUser, { name });
+
+    res.send(foundUser);
+  });
+
+  app.get('/expenses', (req, res) => {
+    const { userId, category, to, from } = req.query;
+
+    if (from && to) {
+      const foundExpensesByDate = expenses.filter(
+        (expense) => expense.spentAt > from
+          && expense.spentAt < to
+      );
+
+      res.send(foundExpensesByDate);
+      res.statusCode = 200;
+
+      return;
+    }
+
+    if (category) {
+      const foundExpensesByCategory = expenses.filter(
+        (expense) =>
+          expense.userId === +userId
+          && expense.category === category
+      );
+
+      res.send(foundExpensesByCategory);
+      res.statusCode = 200;
+
+      return;
+    }
+
+    if (userId) {
+      const foundExpensesByUserId = expenses.filter(
+        (expense) => expense.userId === +userId
+      );
+
+      res.send(foundExpensesByUserId);
+      res.statusCode = 200;
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    res.send(expenses);
+  });
+
+  app.get('/expenses/:expenseId', (req, res) => {
+    const { expenseId } = req.params;
+
+    if (typeof +expenseId !== 'number') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const foundExpense = expenses.find(expense => expense.id === +expenseId);
+
+    if (!foundExpense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    res.statusCode = 200;
+
+    res.send(foundExpense);
+  });
+
+  app.post('/expenses', express.json(), (req, res) => {
+    const {
+      userId,
+      spentAt,
+      title,
+      amount,
+      category,
+      note,
+    } = req.body;
+
+    const newExpense = {
+      userId,
+      spentAt,
+      title,
+      amount,
+      category,
+      id: Math.max(0, ...users.map(({ id }) => id + 1)),
+      note,
+    };
+
+    const foundUser = users.find((user) => user.id === +userId);
+
+    if (!foundUser) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    expenses.push(newExpense);
+
+    res.statusCode = 201;
+
+    res.send(newExpense);
+  });
+
+  app.delete('/expenses/:expenseId', (req, res) => {
+    const { expenseId } = req.params;
+
+    const filteredExpenses = expenses
+      .filter(expense => expense.id !== +expenseId);
+
+    if (filteredExpenses.length === expenses.length) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    expenses = filteredExpenses;
+
+    res.sendStatus(204);
+  });
+
+  app.patch('/expenses/:expenseId', express.json(), (req, res) => {
+    const { expenseId } = req.params;
+    const { title } = req.body;
+
+    const foundExpense = expenses.find(expense => expense.id === +expenseId);
+
+    if (!foundExpense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    if (typeof title !== 'string') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    Object.assign(foundExpense, { title });
+
+    res.send(foundExpense);
+  });
+
+  return app;
 }
 
 module.exports = {
--- tests/expense.test.js
+++ tests/expense.test.js
@@ -13,7 +13,7 @@ describe('Expense', () => {
   });
 
   describe('createExpense', () => {
-    it('should create a new expense', async() => {
+    it('should create a new expense', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -44,14 +44,14 @@ describe('Expense', () => {
         );
     });
 
-    it('should return 400 if name is not provided', async() => {
+    it('should return 400 if name is not provided', async () => {
       await api
         .post('/expenses')
         .send({})
         .expect(400);
     });
 
-    it('should return 400 if user not found', async() => {
+    it('should return 400 if user not found', async () => {
       const expenseData = {
         userId: 1,
         spentAt: '2022-10-19T11:01:43.462Z',
@@ -69,7 +69,7 @@ describe('Expense', () => {
   });
 
   describe('getExpenses', () => {
-    it('should return empty array if no expenses', async() => {
+    it('should return empty array if no expenses', async () => {
       const response = await api
         .get('/expenses')
         .expect(200)
@@ -79,7 +79,7 @@ describe('Expense', () => {
         .toEqual([]);
     });
 
-    it('should return all expenses', async() => {
+    it('should return all expenses', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -113,7 +113,7 @@ describe('Expense', () => {
         ]);
     });
 
-    it('should return all expenses for a user', async() => {
+    it('should return all expenses for a user', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -160,7 +160,7 @@ describe('Expense', () => {
         ]);
     });
 
-    it('should return all expenses between dates', async() => {
+    it('should return all expenses between dates', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -201,7 +201,7 @@ describe('Expense', () => {
         ]);
     });
 
-    it('should return all expenses by category', async() => {
+    it('should return all expenses by category', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -244,7 +244,7 @@ describe('Expense', () => {
   });
 
   describe('getExpense', () => {
-    it('should return expense', async() => {
+    it('should return expense', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -276,7 +276,7 @@ describe('Expense', () => {
         });
     });
 
-    it('should return 404 if expense not found', async() => {
+    it('should return 404 if expense not found', async () => {
       await api
         .get('/expenses/1')
         .expect(404);
@@ -284,7 +284,7 @@ describe('Expense', () => {
   });
 
   describe('updateExpense', () => {
-    it('should update expense', async() => {
+    it('should update expense', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -320,7 +320,7 @@ describe('Expense', () => {
         });
     });
 
-    it('should return 404 if expense not found', async() => {
+    it('should return 404 if expense not found', async () => {
       await api
         .patch('/expenses/1')
         .send({})
@@ -329,7 +329,7 @@ describe('Expense', () => {
   });
 
   describe('deleteExpense', () => {
-    it('should delete expense', async() => {
+    it('should delete expense', async () => {
       const { body: { id: userId } } = await api
         .post('/users')
         .send({
@@ -358,7 +358,7 @@ describe('Expense', () => {
         .expect(404);
     });
 
-    it('should return 404 if expense not found', async() => {
+    it('should return 404 if expense not found', async () => {
       await api
         .delete('/expenses/1')
         .expect(404);
