cdc71d8a066d18806649f6741f4b8d4590e80e7b
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-10-21 07:29:21 +0300

    Users requests implemented

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,91 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+// const cors = require('cors');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  let users = [
+    // {
+    //   id: 1,
+    //   name: 'string',
+    // },
+  ];
+
+  app.get('/users', (req, res) => {
+    res.send(users);
+  });
+
+  app.get('/users/:userId', (req, res) => {
+    const { userId } = req.params;
+
+    if (isNaN(+userId)) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const foundUser = users.find(({ id }) => +userId === id);
+
+    foundUser ? res.send(foundUser) : res.sendStatus(404);
+  });
+
+  app.post('/users', express.json(), (req, res) => {
+    const { name } = req.body;
+
+    if (!name || typeof name !== 'string') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const newId = Math.max(0, ...users.map(({ id }) => id)) + 1;
+    const newUser = {
+      id: newId,
+      name,
+    };
+
+    users.push(newUser);
+
+    res.statusCode = 201;
+    res.send(newUser);
+  });
+
+  app.delete('/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    const filteredUsers = users.filter(({ id }) => +userId !== id);
+    const isUserFound = users.length !== filteredUsers.length;
+
+    users = filteredUsers;
+
+    res.sendStatus(isUserFound ? 204 : 404);
+  });
+
+  app.patch('/users/:userId', express.json(), (req, res) => {
+    const { userId } = req.params;
+    const { name } = req.body;
+
+    if (isNaN(+userId) || !name || typeof name !== 'string') {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const foundUser = users.find(({ id }) => +userId === id);
+
+    if (!foundUser) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    Object.assign(foundUser, { name });
+
+    res.send(foundUser);
+  });
+
+  return app;
 }
 
 module.exports = {
