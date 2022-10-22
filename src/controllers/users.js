56e896b83a115b290fa28afd40072aff405a06f5
roma-shafihullin <lanister0789@gmail.com>
2022-10-23 00:40:27 +0300

    users endpoints were created, routes and controllers were splited

---
+++ src/controllers/users.js
@@ -0,0 +1,81 @@
+'use strict';
+
+let users = [];
+
+const getUsers = (req, res) => {
+  res.send(users);
+};
+
+const createUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+  }
+
+  const id = Math.floor(Date.now() * Math.random());
+
+  const userWithID = {
+    id,
+    name,
+  };
+
+  users.push(userWithID);
+  res.status(201);
+  res.send(userWithID);
+};
+
+const getUser = (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find(user => user.id === +id);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(foundUser);
+};
+
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find(user => user.id === +id);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+  users = users.filter(user => +id !== user.id);
+  res.sendStatus(204);
+};
+
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const targetUser = users.find(user => user.id === +id);
+
+  if (!targetUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  if (name) {
+    targetUser.name = name;
+  }
+
+  res.send(targetUser);
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+};
--- src/createServer.js
+++ src/createServer.js
@@ -1,12 +1,28 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users.js');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(bodyParser.json());
+
+  app.use('/users', usersRoutes);
+
+  app.get('/', (req, res) => {
+    res.send('Hello from Homepage');
+  });
+
+  app.listen(2500, () => {
+    console.log(`http://localhost:2500`);
+  });
+
+  return app;
 }
+
+createServer();
 
 module.exports = {
   createServer,
