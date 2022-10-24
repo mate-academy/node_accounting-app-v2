99453b15d043b8eee0c849f8001770547291c069
Anastasiia Khmelovska <hmelanast1@gmail.com>
2022-10-24 23:56:03 +0300

    final Solution

--- src/createServer.js
+++ src/createServer.js
@@ -2,31 +2,15 @@
 
 const express = require('express');
 const cors = require('cors');
-const { initUsersRoutes } = require('./users.service');
-const { initExpencesRoutes } = require('./expensess.service');
+const { usersRoutes } = require('./usersRoutes');
+const { expencesRoutes } = require('./expensesRoutes');
 
 function createServer() {
   const app = express();
 
-  app.use(express.json());
   app.use(cors());
-
-  const userRouter = express.Router();
-  const expenseRouter = express.Router();
-
-  const users = [];
-
-  const expenses = [];
-  const storage = {
-    users,
-    expenses,
-  };
-
-  initUsersRoutes(userRouter, storage);
-  initExpencesRoutes(expenseRouter, storage);
-
-  app.use('/users', userRouter);
-  app.use('/expenses', expenseRouter);
+  usersRoutes(app);
+  expencesRoutes(app);
 
   return app;
 }
