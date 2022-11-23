e7f62ee89819bf5ababa6b5fd5271fdeb1ba96c9
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-11-23 13:04:17 +0200

    Server was created

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,54 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const { UserController } = require('./controllers/users.js');
+const { ExpensesController } = require('./controllers/expenses.js');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(express.json());
+
+  const userController = new UserController();
+  const expensesController = new ExpensesController();
+
+  app.post('/users', userController.add.bind(userController));
+
+  app.get('/users', userController.getAll.bind(userController));
+
+  app.get('/users/:id', userController.getOne.bind(userController));
+
+  app.patch('/users/:id', userController.change.bind(userController));
+
+  app.delete('/users/:id', userController.remove.bind(userController));
+
+  app.post('/expenses', (req, res) => {
+    const { userId } = req.body;
+    const foundUser = userController.usersService.getUserById(userId);
+
+    if (!foundUser) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const newExpense = expensesController.expensesService.create(req.body);
+
+    res.status(201);
+    res.send(newExpense);
+  });
+
+  app.get('/expenses', expensesController.getAll.bind(expensesController));
+
+  app.get('/expenses/:id', expensesController.getOne.bind(expensesController));
+
+  app.patch('/expenses/:id',
+    expensesController.change.bind(expensesController));
+
+  app.delete('/expenses/:id',
+    expensesController.remove.bind(expensesController));
+
+  return app;
 }
 
 module.exports = {
