1796a4a0e33541a61e13f1e72d449256524411e5
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-11-28 18:56:03 +0200

    additional fixes

--- src/createServer.js
+++ src/createServer.js
@@ -4,13 +4,11 @@
 const express = require('express');
 const { router: expensesRouter } = require('./routes/expenses');
 const { router: usersRouter } = require('./routes/users');
-const cors = require('cors');
 
 function createServer() {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
 
   app.use('/users', usersRouter);
   app.use('/expenses', expensesRouter);
