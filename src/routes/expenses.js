1f262ab795ae2fdaad2272e62db6f142ba6fb1c1
Misha Dzhemula <medvedmisha23@gmail.com>
2022-12-08 21:09:37 +0200

    refactored

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -6,13 +6,9 @@ const expenseController = require('../controllers/expenses');
 const router = express.Router();
 
 router.get('/', expenseController.getAll);
-
 router.get('/:id', expenseController.getOne);
-
 router.post('/', expenseController.add);
-
 router.delete('/:id', expenseController.remove);
-
 router.patch('/:id', expenseController.update);
 
 module.exports = {
--- src/routes/users.js
+++ src/routes/users.js
@@ -6,13 +6,9 @@ const userController = require('../controllers/users');
 const router = express.Router();
 
 router.get('/', userController.getAll);
-
 router.get('/:id', userController.getOne);
-
 router.post('/', userController.add);
-
 router.delete('/:id', userController.remove);
-
 router.patch('/:id', userController.update);
 
 module.exports = {
