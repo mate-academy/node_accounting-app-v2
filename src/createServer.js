015280e8b293dc27b7c5c2f4db00b663492d044a
Andriy Shymkiv <winamondezik@gmail.com>
2022-12-31 11:48:45 +0200

    router was added

--- src/createServer.js
+++ src/createServer.js
@@ -2,11 +2,11 @@
 
 const express = require('express');
 
+const usersRouter = require('./routes/users');
+const expensesRouter = require('./routes/expenses');
+
 const usersService = require('./services/users');
 const expensesService = require('./services/expenses');
-
-const usersController = require('./controllers/users');
-const expensesController = require('./controllers/expenses');
 
 function createServer() {
   const app = express();
@@ -14,25 +14,8 @@ function createServer() {
   usersService.init();
   expensesService.init();
 
-  app.get('/users', usersController.getAll);
-
-  app.post('/users', express.json(), usersController.addOne);
-
-  app.get('/users/:userId', usersController.getOne);
-
-  app.delete('/users/:userId', usersController.deleteOne);
-
-  app.patch('/users/:userId', express.json(), usersController.updateOne);
-
-  app.get('/expenses', expensesController.getAll);
-
-  app.post('/expenses', express.json(), expensesController.addOne);
-
-  app.get('/expenses/:expenseId', expensesController.getOne);
-
-  app.delete('/expenses/:expenseId', expensesController.deleteOne);
-
-  app.patch('/expenses/:expenseId', express.json(), expensesController.updateOne);
+  app.use('/users', usersRouter);
+  app.use('/expenses', expensesRouter);
 
   return app;
 }
