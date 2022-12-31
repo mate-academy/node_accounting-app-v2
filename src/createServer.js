f4f72e442291e908307057c0a911e9607d202f89
Andriy Shymkiv <winamondezik@gmail.com>
2022-12-31 11:25:18 +0200

    controllers was created

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,12 @@
 'use strict';
 
 const express = require('express');
+
 const usersService = require('./services/users');
 const expensesService = require('./services/expenses');
+
+const usersController = require('./controllers/users');
+const expensesController = require('./controllers/expenses');
 
 function createServer() {
   const app = express();
@@ -10,157 +14,25 @@ function createServer() {
   usersService.init();
   expensesService.init();
 
-  app.get('/users', express.json(), (req, res) => {
-    const users = usersService.getAll();
+  app.get('/users', usersController.getAll);
 
-    res.send(users);
-  });
+  app.post('/users', express.json(), usersController.addOne);
 
-  app.post('/users', express.json(), (req, res) => {
-    const { name } = req.body;
+  app.get('/users/:userId', usersController.getOne);
 
-    if (!name) {
-      res.sendStatus(400);
+  app.delete('/users/:userId', usersController.deleteOne);
 
-      return;
-    };
+  app.patch('/users/:userId', express.json(), usersController.updateOne);
 
-    const newUser = usersService.addOne(name);
+  app.get('/expenses', expensesController.getAll);
 
-    res.statusCode = 201;
-    res.send(newUser);
-  });
+  app.post('/expenses', express.json(), expensesController.addOne);
 
-  app.get('/users/:userId', express.json(), (req, res) => {
-    const { userId } = req.params;
+  app.get('/expenses/:expenseId', expensesController.getOne);
 
-    const foundUser = usersService.getOne(userId);
+  app.delete('/expenses/:expenseId', expensesController.deleteOne);
 
-    if (!foundUser) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    res.statusCode = 200;
-    res.send(foundUser);
-  });
-
-  app.delete('/users/:userId', express.json(), (req, res) => {
-    const { userId } = req.params;
-
-    const foundUser = usersService.getOne(userId);
-
-    if (!foundUser) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    usersService.deleteOne(userId);
-    res.sendStatus(204);
-  });
-
-  app.patch('/users/:userId', express.json(), (req, res) => {
-    const { userId } = req.params;
-    const { name } = req.body;
-
-    const foundUser = usersService.getOne(userId);
-
-    if (!foundUser) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    const updatedUser = usersService.updateOne(userId, name);
-
-    res.send(updatedUser);
-  });
-
-  app.get('/expenses', express.json(), (req, res) => {
-    const { userId, category, from, to } = req.query;
-    const foundExpenses = expensesService.getAll(userId, category, from, to);
-
-    res.send(foundExpenses);
-  });
-
-  app.post('/expenses', express.json(), (req, res) => {
-    const {
-      userId,
-      spentAt,
-      title,
-      amount,
-      category,
-      note,
-    } = req.body;
-
-    const foundUser = usersService.getOne(userId);
-
-    if (!foundUser) {
-      res.sendStatus(400);
-
-      return;
-    }
-
-    const newExpense = expensesService.addOne(
-      userId,
-      spentAt,
-      title,
-      amount,
-      category,
-      note,
-    );
-
-    res.statusCode = 201;
-    res.send(newExpense);
-  });
-
-  app.get('/expenses/:expenseId', express.json(), (req, res) => {
-    const { expenseId } = req.params;
-
-    const foundExpense = expensesService.getOne(expenseId);
-
-    if (!foundExpense) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    res.send(foundExpense);
-  });
-
-  app.delete('/expenses/:expenseId', express.json(), (req, res) => {
-    const { expenseId } = req.params;
-
-    const foundExpense = expensesService.getOne(expenseId);
-
-    if (!foundExpense) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    expensesService.deleteOne(expenseId);
-    res.sendStatus(204);
-  });
-
-  app.patch('/expenses/:expenseId', express.json(), (req, res) => {
-    const { expenseId } = req.params;
-    const { title } = req.body;
-
-    let foundExpense = expensesService.getOne(expenseId);
-
-    if (!foundExpense) {
-      res.sendStatus(404);
-
-      return;
-    }
-
-    foundExpense = expensesService.updateOne(expenseId, title);
-
-    res.send(foundExpense);
-  });
+  app.patch('/expenses/:expenseId', express.json(), expensesController.updateOne);
 
   return app;
 }
