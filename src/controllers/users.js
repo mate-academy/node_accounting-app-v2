4f1355fd6666a070b97ebc129b9dbb0bfbfeb666
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2023-01-25 00:28:05 +0200

    solution

---
+++ src/controllers/users.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const usersService = require('../services/users');
+
+const getAll = (req, res) => {
+  const users = usersService.getAll();
+
+  res.send(users);
+};
+
+const getOne = (req, res) => {
+  const { userId } = req.params;
+
+  const foundUser = usersService.getbyId(userId);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(foundUser);
+};
+
+const add = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const newUser = usersService.create(name);
+
+  res.statusCode = 201;
+  res.send(newUser);
+};
+
+const remove = (req, res) => {
+  const { userId } = req.params;
+  const foundUser = usersService.getbyId(userId);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  usersService.remove(userId);
+  res.sendStatus(204);
+};
+
+const update = (req, res) => {
+  const { userId } = req.params;
+  const foundUser = usersService.getbyId(userId);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const { name } = req.body;
+
+  if (typeof name !== 'string') {
+    res.status(422);
+
+    return;
+  }
+
+  const updatedUser = usersService.update({
+    id: userId,
+    name,
+  });
+
+  res.send(updatedUser);
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  add,
+  remove,
+  update,
+};
---
+++ src/services/expenses.js
@@ -0,0 +1,76 @@
+'use strict';
+
+let expenses = [];
+
+function init() {
+  expenses = [];
+};
+
+function getAll(userId, category, from, to) {
+  let currentExpenses = expenses;
+
+  if (userId) {
+    currentExpenses = currentExpenses
+      .filter(expense => expense.userId === +userId);
+  }
+
+  if (category) {
+    currentExpenses = currentExpenses
+      .filter(expense => expense.category === category);
+  }
+
+  if (from) {
+    currentExpenses = currentExpenses
+      .filter(expense => expense.spentAt >= from);
+  }
+
+  if (to) {
+    currentExpenses = currentExpenses
+      .filter(expense => expense.spentAt <= to);
+  }
+
+  return currentExpenses;
+};
+
+function getbyId(expenseId) {
+  const foundExpense = expenses.find(expense => expense.id === +expenseId);
+
+  return foundExpense || null;
+}
+
+function create(userId, spentAt, title, amount, category, note) {
+  const newExpense = {
+    id: expenses.length + 1,
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  };
+
+  expenses.push(newExpense);
+
+  return newExpense;
+};
+
+function remove(expenseId) {
+  expenses = expenses.filter(expense => expense.id !== +expenseId);
+};
+
+function update({ id, data }) {
+  const expense = getbyId(id);
+
+  Object.assign(expense, data);
+
+  return expense;
+};
+
+module.exports = {
+  init,
+  getAll,
+  getbyId,
+  create,
+  remove,
+  update,
+};
