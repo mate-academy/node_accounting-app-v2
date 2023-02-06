65168115d7427da13775a1a3a3ebf19f268e0c19
nazarmatsevych <nazar6192111@gmail.com>
2023-02-06 17:55:48 +0200

    remove comments

--- src/routes/expenses.js
+++ src/routes/expenses.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', expenseController.getAll);
 router.get('/:id', expenseController.getOne);
-//
+
 router.post('/', expenseController.add);
 router.delete('/:id', expenseController.remove);
 router.patch('/:id', expenseController.update);
