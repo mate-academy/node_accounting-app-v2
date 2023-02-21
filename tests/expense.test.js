a7f3964d9ed98f91bec7697df70524e44efe6cb1
Vadym Voiedilo <asd4s.profile@gmail.com>
2023-02-21 16:04:39 +0200

    Fixed the line with max-length problem

--- tests/expense.test.js
+++ tests/expense.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 'use strict';
 
 const supertest = require('supertest');
@@ -188,6 +189,7 @@ describe('Expense', () => {
         });
 
       const response = await api
+        // eslint-disable-next-line max-len
         .get(`/expenses?&from=2022-10-19T00:00:00.000Z&to=2022-10-19T23:59:59.999Z`)
         .expect(200)
         .expect('Content-Type', /application\/json/);
