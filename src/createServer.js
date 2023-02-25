fc9c961383994a069827ce4c66f39e3d0d5f4b79
Anton Martynes <martynes1997@gmail.com>
2023-02-25 02:14:31 +0200

    'expenses' was added

--- src/createServer.js
+++ src/createServer.js
@@ -1,15 +1,19 @@
 'use strict';
 
 const express = require('express');
+const { expenseRouter } = require('./routes/expenseRouter');
 const { userRouter } = require('./routes/usersRoute');
+const { deleteAllExpenses } = require('./services/expenseService');
 const { deleteAllUsers } = require('./services/usersService');
 
 function createServer() {
   const app = express();
 
   deleteAllUsers();
+  deleteAllExpenses();
 
   app.use('/users', express.json(), userRouter);
+  app.use('/expenses', express.json(), expenseRouter);
 
   return app;
 }
