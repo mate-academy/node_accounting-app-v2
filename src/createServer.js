3074b23d521639b77663e684fb2d1259d2103c8a
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-10 10:05:24 +0200

    deleted cors module

--- src/createServer.js
+++ src/createServer.js
@@ -5,15 +5,12 @@ const { router: usersRouter } = require('./routes/users');
 const { router: expensesRouter } = require('./routes/expenses');
 const userServices = require('./services/users');
 const expensesServices = require('./services/expenses');
-const cors = require('cors');
 
 const createServer = () => {
   const app = express();
 
   userServices.init();
   expensesServices.init();
-
-  app.use(cors());
 
   app.use('/users', express.json(), usersRouter);
 
