6f45a09e25b490f28d9ff1040a1532c098e3a31c
Oleksandr-Maly <117357713+Oleksandr-Maly@users.noreply.github.com>
2023-03-10 21:19:20 +0200

    solution

---
+++ src/utiles/generateNewId.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const generateNewId = (elements) => {
+  const maxId = Math.max(...elements.map(element => element.id), 0);
+
+  return maxId + 1;
+};
+
+module.exports = generateNewId;
