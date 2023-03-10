2961d2a820f84fe35e1fea1da8abd1da45d6f339
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2023-03-10 12:16:44 +0200

    fixed cors tests

--- src/createServer.js
+++ src/createServer.js
@@ -5,12 +5,15 @@ const { router: usersRouter } = require('./routes/users');
 const { router: expensesRouter } = require('./routes/expenses');
 const userServices = require('./services/users');
 const expensesServices = require('./services/expenses');
+const cors = require('cors');
 
 const createServer = () => {
   const app = express();
 
   userServices.init();
   expensesServices.init();
+
+  app.use(cors());
 
   app.use('/users', express.json(), usersRouter);
 
