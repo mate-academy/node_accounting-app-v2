fce2e44e16e4f1a484b10ee8805ab9fac1e8d9ed
Vlad Horovyi <vlad78292@gmail.com>
2023-03-13 02:29:30 +0200

    controllers were added

--- src/controllers/expenses.js
+++ src/controllers/expenses.js
@@ -2,3 +2,129 @@
 
 const expenseService = require('../service/expenses');
 
+const getAll = (req, res) => {
+  const filterBy = req.query;
+
+  if (!filterBy) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(expenseService(filterBy));
+};
+
+const getExpense = (req, res) => {
+  let { expenseId } = req.params;
+
+  expenseId = +expenseId;
+
+  if (isNaN(expenseId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const foundExpense = expenseService.getExpense(expenseId);
+
+  if (!foundExpense) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(foundExpense);
+};
+
+const deleteExpense = (req, res) => {
+  let { expenseId } = req.params;
+
+  expenseId = +expenseId;
+
+  if (isNaN(expenseId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const remainedExpenses = expenseService.deleteExpense(expenseId);
+
+  if (remainedExpenses.length === expenseService.getAll.length) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.sendStatus(204);
+};
+
+const updateExpense = (req, res) => {
+  let { expenseId } = req.params;
+
+  expenseId = +expenseId;
+
+  if (isNaN(expenseId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const foundExpense = expenseService.getExpense(expenseId);
+
+  if (!foundExpense) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const updatedExpense = expenseService.updateExpense(expenseId, ...req.body);
+
+  res.send(updatedExpense);
+};
+
+const addNewExpense = (req, res) => {
+  const { userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note } = req.body;
+
+  if (!userId || !spentAt || !title || !amount || !category || !note) {
+    res.sendStatus(400);
+
+    return;
+  };
+
+  if (!getAll().userId.includes(userId)) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  if (
+    typeof userId !== 'number'
+    || typeof spentAt !== 'string'
+    || typeof title !== 'string'
+    || typeof amount !== 'number'
+    || typeof category !== 'string'
+    || note !== 'string'
+  ) {
+    res.sendStatus(400);
+
+    return;
+  };
+
+  const newExpense = expenseService.addExpense(req.body);
+
+  res.status(201);
+  res.send(newExpense);
+};
+
+module.exports = {
+  getAll,
+  getExpense,
+  deleteExpense,
+  addNewExpense,
+  updateExpense,
+};
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,3 +2,103 @@
 
 const userService = require('../service/users');
 
+const getAll = (req, res) => {
+  const users = userService.getAll();
+
+  res.send(users);
+};
+
+const getUser = (req, res) => {
+  let { userId } = req.params;
+
+  userId = +userId;
+
+  if (isNaN(userId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const foundUser = userService.getUser(userId);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.status(201);
+  res.send(foundUser);
+};
+
+const updateUser = (req, res) => {
+  let { userId } = req.params;
+
+  userId = +userId;
+
+  if (isNaN(userId)) {
+    res.sendStatus(404);
+
+    return;
+  };
+
+  const foundUser = userService.getUser(userId);
+
+  if (!foundUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const { name } = req.body;
+
+  const newUser = userService.updateUser(userId, name);
+
+  res.status(201);
+  res.send(newUser);
+};
+
+const deleteUser = (req, res) => {
+  let { userId } = req.params;
+
+  userId = +userId;
+
+  if (isNaN(userId)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const usersAfterDelete = userService.deleteUser(userId);
+
+  if (usersAfterDelete.length === userService.getAll().length) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.sendStatus(201);
+};
+
+const addUser = (res, req) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(404);
+
+    return;
+  };
+
+  const addedUser = userService.addUser(name);
+
+  res.status(201);
+  res.send(addedUser);
+};
+
+module.exports = {
+  getAll,
+  getUser,
+  updateUser,
+  deleteUser,
+  addUser,
+};
