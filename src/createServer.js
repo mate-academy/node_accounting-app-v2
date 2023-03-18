5e557c2af3c2ef4ba993134eaab6ffdf4492efd5
Oleh Yablunovskyi <cowperwood777@gmail.com>
2023-03-18 16:42:26 +0200

    Expenses endpoints were implemented

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 const express = require('express');
@@ -6,10 +7,182 @@ function getRandomDigits() {
   return +Math.random().toString().slice(2);
 }
 
+function validateExpenseCreation(body) {
+  const allowedFields = [
+    'userId', 'spentAt', 'title', 'amount', 'category', 'note',
+  ];
+  const receivedFields = Object.keys(body);
+
+  // Check if every field is allowed
+  const isValidOperation = receivedFields.every(
+    field => allowedFields.includes(field)
+  );
+
+  if (!isValidOperation) {
+    return false;
+  }
+
+  // Check that fields have allowed types
+  if (
+    isNaN(Date.parse(body.spentAt))
+    && typeof body.title !== 'string'
+    && typeof body.amount !== 'number'
+    && typeof body.category !== 'string'
+    && typeof body.note !== 'string'
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
+function validateExpenseUpdate(body) {
+  const allowedFields = [
+    'userId', 'spentAt', 'title', 'amount', 'category', 'note',
+  ];
+  const receivedFields = Object.keys(body);
+
+  // Check if every field is allowed
+  const isValidOperation = receivedFields.every(
+    update => allowedFields.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return false;
+  }
+
+  // Check that fields have allowed types if present
+  if (
+    (body.userId !== undefined && typeof body.userId !== 'number')
+    || (body.spentAt !== undefined && isNaN(Date.parse(body.spentAt)))
+    || (body.title !== undefined && typeof body.title !== 'string')
+    || (body.amount !== undefined && typeof body.amount !== 'number')
+    || (body.category !== undefined && typeof body.category !== 'string')
+    || (body.note !== undefined && typeof body.note !== 'string')
+  ) {
+    return false;
+  }
+
+  return true;
+}
+
 function createServer() {
+  let expenses = [];
   let users = [];
   const app = express();
 
+  // expenses endpoints
+  app.get('/expenses', (req, res) => {
+    const {
+      userId,
+      category,
+      from,
+      to,
+    } = req.query;
+
+    const filteredExpenses = expenses.filter(expense => {
+      const isUserIdMatch = userId
+        ? expense.userId === +userId
+        : true;
+
+      const isCategoryMatch = category
+        ? expense.category === category
+        : true;
+
+      const isFromMatch = from
+        ? expense.spentAt >= from
+        : true;
+
+      const isToMatch = to
+        ? expense.spentAt <= to
+        : true;
+
+      return isUserIdMatch && isCategoryMatch && isFromMatch && isToMatch;
+    });
+
+    res.send(filteredExpenses);
+  });
+
+  app.post('/expenses', express.json(), (req, res) => {
+    const { userId } = req.body;
+    const foundUser = users.find(user => user.id === +userId);
+
+    if (!foundUser) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const isBodyValid = validateExpenseCreation(req.body);
+
+    if (!isBodyValid) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const newExpense = {
+      id: getRandomDigits(),
+      ...req.body,
+    };
+
+    expenses.push(newExpense);
+
+    res.statusCode = 201;
+    res.send(newExpense);
+  });
+
+  app.get('/expenses/:id', (req, res) => {
+    const { id } = req.params;
+    const foundExpense = expenses.find(expense => expense.id === +id);
+
+    if (!foundExpense) {
+      res.status(404).send('Expense does not exist');
+
+      return;
+    }
+
+    res.send(foundExpense);
+  });
+
+  app.delete('/expenses/:id', (req, res) => {
+    const { id } = req.params;
+    const filteredExpenses = expenses.filter(expense => expense.id !== +id);
+
+    if (filteredExpenses.length === expenses.length) {
+      res.status(404).send('User does not exist');
+
+      return;
+    }
+
+    expenses = filteredExpenses;
+    res.sendStatus(204);
+  });
+
+  app.patch('/expenses/:id', express.json(), (req, res) => {
+    const { id } = req.params;
+    const foundExpense = expenses.find(expense => expense.id === +id);
+
+    if (!foundExpense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    const isBodyValid = validateExpenseUpdate(req.body);
+
+    if (!isBodyValid) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    Object.assign(foundExpense, { ...req.body });
+
+    res.send(foundExpense);
+  });
+
+  // users endpoints
   app.get('/users', (req, res) => {
     res.send(users);
   });
@@ -62,19 +235,19 @@ function createServer() {
   });
 
   app.patch('/users/:id', express.json(), (req, res) => {
+    const { name } = req.body;
+
+    if (!name || typeof name !== 'string') {
+      res.sendStatus(400);
+
+      return;
+    }
+
     const { id } = req.params;
     const foundUser = users.find(user => user.id === +id);
 
     if (!foundUser) {
       res.sendStatus(404);
-
-      return;
-    }
-
-    const { name } = req.body;
-
-    if (typeof name !== 'string') {
-      res.sendStatus(422);
 
       return;
     }
