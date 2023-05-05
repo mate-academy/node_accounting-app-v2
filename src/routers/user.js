e7d17304ef4b4ecbb4a7c437cc49b0144e413135
timoxa2t <alex2t172@gmail.com>
2023-05-05 17:46:20 +0300

    MVP implemented

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,83 +1,19 @@
 'use strict';
 
 const express = require('express');
-const userService = require('../services/user');
+const userController = require('../controllers/user');
 const router = express.Router();
 
 router.use(express.json());
 
-router.get('/', (req, res) => {
-  const users = userService.getUsers();
+router.get('/', userController.getAllUsers);
 
-  res.json(users);
-});
+router.get('/:userId', userController.getUserById);
 
-router.get('/:userId', (req, res) => {
-  const { userId } = req.params;
+router.delete('/:userId', userController.deleteUserById);
 
-  const foundUser = userService.getUserById(userId);
+router.patch('/:userId', userController.updateUserById);
 
-  if (!foundUser) {
-    res.sendStatus(404);
-
-    return;
-  }
-
-  res.json(foundUser);
-});
-
-router.delete('/:userId', (req, res) => {
-  const { userId } = req.params;
-
-  const foundUser = userService.getUserById(userId);
-
-  if (!foundUser) {
-    res.sendStatus(404);
-
-    return;
-  }
-
-  userService.removeUserById(userId);
-
-  res.sendStatus(204);
-});
-
-router.patch('/:userId', (req, res) => {
-  const { userId } = req.params;
-  const { name } = req.body;
-
-  if (!name) {
-    res.sendStatus(400);
-
-    return;
-  }
-
-  const foundUser = userService.getUserById(userId);
-
-  if (!foundUser) {
-    res.sendStatus(404);
-
-    return;
-  }
-
-  const updated = userService.patchUser(foundUser, { name });
-
-  res.json(updated);
-});
-
-router.post('/', (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    res.sendStatus(400);
-
-    return;
-  }
-
-  const createdUser = userService.addUser({ name });
-
-  res.status(201);
-  res.json(createdUser);
-});
+router.post('/', userController.createUser);
 
 module.exports = router;
