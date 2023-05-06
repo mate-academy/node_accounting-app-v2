896f2d9670e3d924168bd9ff536ef3eac55a6154
Maksym <maksymmaliuk98@gmail.com>
2023-05-06 19:42:06 +0300

    task done

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,23 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const { router: users } = require('./routes/users');
+const { router: expenses } = require('./routes/expense');
+const { userService } = require('./services/userService');
+const { expenseService } = require('./services/expenseService');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(express.json());
+
+  app.use('/users', express.json(), users);
+  app.use('/expenses', express.json(), expenses);
+
+  userService.clearAll();
+  expenseService.clearAll();
+
+  return app;
 }
 
 module.exports = {
---
+++ src/services/expenseService.js
@@ -0,0 +1,48 @@
+'use strict';
+
+class Expenses {
+  constructor() {
+    this.expenses = [];
+  }
+
+  clearAll() {
+    this.expenses = [];
+  }
+
+  getAll() {
+    return this.expenses;
+  }
+
+  getById(id) {
+    const foundExpense = this.expenses.find(expense => (
+      expense.id === +id
+    ));
+
+    return foundExpense || null;
+  }
+
+  create(expenseData) {
+    const newId = this.expenses.length
+      ? Math.max(...this.expenses.map(expense => expense.id))
+      : 0;
+
+    const newExpense = {
+      ...expenseData,
+      id: newId + 1,
+    };
+
+    this.expenses.push(newExpense);
+
+    return newExpense;
+  }
+
+  remove(id) {
+    this.expenses = this.expenses.filter(expense => expense.id !== +id);
+  }
+
+  update(expense, updatedData) {
+    Object.assign(expense, updatedData);
+  };
+}
+
+module.exports = { expenseService: new Expenses() };
