4ee5131a709c9278cdfa9997bf8d659540655d23
Rostyslav Horytskyi <gorzer.rostyslav@gmail.com>
2023-05-25 15:35:57 +0300

    create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,26 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+
+const { usersService } = require('./services/users');
+const { expensesService } = require('./services/expenses');
+
+const usersRouter = require('./routes/users');
+const expensesRouter = require('./routes/expenses');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  usersService.reset();
+  expensesService.reset();
+
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use('/users', usersRouter);
+  app.use('/expenses', expensesRouter);
+
+  return app;
 }
 
 module.exports = {
