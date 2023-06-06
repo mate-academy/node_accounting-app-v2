d775f7f55aa05d61dbb75cfbe4ea876793afbfda
Hamuud <artemlask2014@gmail.com>
2023-06-06 14:13:28 +0300

    Change comment line in createServer

--- src/createServer.js
+++ src/createServer.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// All i need is cry all tonight... still now worked ;(
+// it works, but i still want to cry ;(, also Dmytro Hrytsak say hello to you
 
 const express = require('express');
 const { userRouter } = require('./routes/userRouter');
