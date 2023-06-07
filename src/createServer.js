c5cc004ce29da9b0c2dd1ce2482890e66afb1272
Yaroslav <kbshnik2020@gmail.com>
2023-06-07 23:28:08 +0300

    almost done, trying to fix bugs

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,26 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+
+const { userRouter } = require('./routes/userRouter');
+const { expenseRouter } = require('./routes/expenseRouter');
+const { resetUsers } = require('./servises/userServices');
+const { removeExpense } = require('./servises/expenseServices');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  resetUsers();
+  removeExpense();
+
+  return app;
 }
 
 module.exports = {
---
+++ src/routes/userRouter.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const userController = require('../conrollers/userController');
+
+const userRouter = express.Router();
+
+userRouter.get('/', userController.getUsers);
+
+userRouter.get('/:id', userController.getUserById);
+
+userRouter.post('/', userController.createUser);
+
+userRouter.delete('/:id', userController.deleteUser);
+
+userRouter.patch('/:id', userController.updateUser);
+
+module.exports = {
+  userRouter,
+};
