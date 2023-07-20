23e0e6768068f09ab2da822c4a84ab50699757fc
Denys Didenko <ddidenko441@gmail.com>
2023-07-20 22:22:59 +0300

    add solution

---
+++ src/routes/expenses.js
@@ -0,0 +1,146 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const { users } = require('./users');
+
+let expenses = [];
+
+router.get('/', (req, res) => {
+  const {
+    userId,
+    categories,
+    from,
+    to,
+  } = req.query;
+
+  if (userId) {
+    expenses = expenses.filter(expense => (
+      expense.userId === userId));
+  }
+
+  if (categories) {
+    expenses = expenses.filter(expense => (
+      categories.includes(expense.category)
+    ));
+  }
+
+  if (from) {
+    expenses = expenses.filter(expense => (
+      expense.spentAt >= from
+    ));
+  }
+
+  if (to) {
+    expenses = expenses.filter(expense => (
+      expense.spentAt <= to
+    ));
+  }
+
+  if (from && to) {
+    expenses = expenses.filter(expense => (
+      expense.spentAt >= from && expense.spentAt <= to
+    ));
+  }
+
+  res.send(expenses);
+});
+
+router.post('/', (req, res) => {
+  const {
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  } = req.body;
+
+  /* const isAllValuesExist = userId && spentAt && title && amount && category;
+
+  if (!isAllValuesExist) {
+    res.sendStatus(400);
+
+    return;
+  } */
+
+  const findUser = users.find(user => user.id === userId);
+
+  if (!findUser) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const newExpense = {
+    id: Math.floor(Math.random() * 100),
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  };
+
+  expenses.push(newExpense);
+
+  res.sendStatus(201);
+  res.send(newExpense);
+});
+
+router.get('/:expenseId', (req, res) => {
+  const { expenseId } = req.params;
+
+  const findExpense = expenses.find(expense => expense.id === +expenseId);
+
+  if (!findExpense) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(findExpense);
+});
+
+router.patch('/:expenseId', (req, res) => {
+  const { expenseId } = req.params;
+
+  const findExpense = expenses.find(expense => expense.id === expenseId);
+
+  if (!findExpense) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const updatedExpense = {
+    ...findExpense,
+    ...req.body,
+  };
+
+  Object.assign(findExpense, updatedExpense);
+
+  res.send(findExpense);
+});
+
+router.delete('/:expenseId', (req, res) => {
+  const { expenseId } = req.params;
+
+  const filteredExpenses = expenses.filter(expense => expense.id !== expenseId);
+
+  if (filteredExpenses.length === expenses.length) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  expenses = filteredExpenses;
+
+  res.sendStatus(204);
+});
+
+module.exports = {
+  router,
+  users,
+  expenses,
+};
---
+++ src/routes/users.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+let users = [];
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.post('/', (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  const newUser = {
+    id: Math.floor(Math.random() * 100),
+    name,
+  };
+
+  users.push(newUser);
+
+  res.sendStatus(201);
+  res.send(newUser);
+});
+
+router.get('/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  const findUser = users.find(user => user.id === userId);
+
+  if (!findUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(findUser);
+});
+
+router.delete('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const filteredUsers = users.filter(user => user.id !== userId);
+
+  if (users.length === filteredUsers.length) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  users = filteredUsers;
+
+  res.sendStatus(204);
+});
+
+router.patch('/:userId', (req, res) => {
+  const { userId } = req.params;
+  const { name } = req.body;
+
+  const findUser = users.find(user => user.id === userId);
+
+  if (!findUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  if (!name) {
+    res.sendStatus(422);
+
+    return;
+  }
+
+  Object.assign(findUser, { name });
+
+  res.send(findUser);
+});
+
+module.exports = {
+  router,
+  users,
+};
