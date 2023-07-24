8e785b6d6936c3ec2916105a2b348ad559913474
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-07-24 21:34:11 +0300

    initial commit

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,51 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const uuid = require('uuid');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  let users = [];
+  let expenses = [];
+
+  app.get('/users', (_, response) => {
+    response.send(users);
+  });
+
+  app.get('/users/:userId', (request, response) => {
+    const { userId } = request.params;
+    const foundUser = users.find(user => user.id === userId);
+
+    if (!foundUser) {
+      response.statusCode(404);
+
+      return;
+    }
+
+    response.statusCode(200);
+    response.send(foundUser);
+  });
+
+  app.post('/users', express.json(), (request, response) => {
+    const { name } = request.body;
+
+    if (!name) {
+      response.sendStatus(400);
+
+      return;
+    }
+
+    const newUser = {
+      id: uuid(),
+      name,
+    };
+
+    users.push(newUser);
+
+    response.statusCode(201);
+    response.send(newUser);
+  });
 }
 
 module.exports = {
