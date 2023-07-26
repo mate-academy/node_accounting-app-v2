94962dc10b239981a742f78c1e481ce4d4a1dd90
Kiril Shorin <kirillshorin8838@gmail.com>
2023-07-26 17:26:41 +0300

    add expenses routes

--- src/createServer.js
+++ src/createServer.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const { router: userRouter } = require('./routes/user');
+const { router: expenseRouter } = require('./routes/expense');
 
 function createServer() {
   const server = express();
 
   server.use('/users', express.json(), userRouter);
+  server.use('/expenses', express.json(), expenseRouter);
 
   return server;
 }
