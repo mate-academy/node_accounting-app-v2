892a5c303f3e1e3264c100899fe28d92c87872a7
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-07-30 01:29:38 +0300

    sol

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,23 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const { router: expensesRouter } = require('./routers/expenses');
+const { router: usersRouter } = require('./routers/users');
+const { clearDatabase: clearExpenses } = require('./services/expenses');
+const { clearDatabase: clearUsers } = require('./services/users');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(express.json());
+
+  clearExpenses();
+  clearUsers();
+
+  app.use('/expenses', expensesRouter);
+  app.use('/users', usersRouter);
+
+  return app;
 }
 
 module.exports = {
