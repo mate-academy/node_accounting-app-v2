615d61af52abd44c2170d43ed7b149a711026cb8
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-08-22 18:21:58 +0300

    Added routes for users and expenses

--- src/createServer.js
+++ src/createServer.js
@@ -2,17 +2,11 @@
 'use strict';
 
 const express = require('express');
-const { getAll, getOne, create, remove, update } = require('./controllers/users.js');
 const { clearUsers } = require('./services/users.service');
 const { clearExpenses } = require('./services/expenses.service');
 
-const {
-  getAllExpenses,
-  getOneExpense,
-  addExpense,
-  removeExpense,
-  patchExpense,
-} = require('./controllers/expenses.js');
+const userRouter = require('./routes/users.js');
+const expenseRouter = require('./routes/expenses.js');
 
 function createServer() {
   const app = express();
@@ -22,17 +16,8 @@ function createServer() {
   clearUsers();
   clearExpenses();
 
-  app.get('/users', getAll);
-  app.get('/users/:id', getOne);
-  app.post('/users', express.json(), create);
-  app.delete('/users/:id', remove);
-  app.patch('/users/:id', express.json(), update);
-
-  app.get('/expenses', getAllExpenses);
-  app.get('/expenses/:id', getOneExpense);
-  app.post('/expenses', express.json(), addExpense);
-  app.delete('/expenses/:id', removeExpense);
-  app.patch('/expenses/:id', express.json(), patchExpense);
+  app.use('/users', express.json(), userRouter);
+  app.use('/expenses', express.json(), expenseRouter);
 
   return app;
 }
