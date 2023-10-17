8f845726571ba20fd567cf8d4f10efca4d5cf1bd
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-17 17:44:11 +0300

    fixed CRUCIAL mastake with services

--- src/createServer.js
+++ src/createServer.js
@@ -3,14 +3,14 @@
 const express = require('express');
 const { usersRouter } = require('./routes/users.routes');
 const { expensesRouter } = require('./routes/expenses.routes');
-const { expensesService } = require('../services/expenses.services');
-const { userService } = require('../services/users.services');
+const { clear: clearExpenses } = require('./services/expenses.services');
+const { clear: clearUsers } = require('./services/users.services');
 
 function createServer() {
   const app = express();
 
-  userService.clear();
-  expensesService.clear();
+  clearExpenses();
+  clearUsers();
 
   app.use('/users', express.json(), usersRouter);
   app.use('/expenses', express.json(), expensesRouter);
