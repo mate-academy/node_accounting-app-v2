bd606cb9b9062c9c45b814147739f9c5e7f73a75
Diana Honcharova <fancypeanutbutter@gmail.com>
2023-10-17 09:18:48 +0300

    usersRouter connected to server

--- src/createServer.js
+++ src/createServer.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const cors = require('cors');
+const { usersRouter } = require('./Routes/users.routes');
 
 function createServer() {
   const app = express();
 
   app.use(cors());
+
+  app.use('/users', express.json(), usersRouter);
 
   return app;
 }
