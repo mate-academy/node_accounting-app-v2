1b6888292bbd80d02fc4ac8f8c0ca5d364d9ed68
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-10-19 21:37:00 +0300

    add fix

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -6,7 +6,7 @@ const usersController = require('../controllers/users.controller');
 const usersRouter = express.Router();
 
 usersRouter.get('/', usersController.getAll);
-usersRouter.get('/:id', usersController.getOne);
+usersRouter.get('/:id', usersController.getById);
 usersRouter.post('/', usersController.create);
 usersRouter.patch('/:id', usersController.update);
 usersRouter.delete('/:id', usersController.remove);
