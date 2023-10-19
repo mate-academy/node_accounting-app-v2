1260d8e0917d498bc47c30d8fbaa3e8a30eebac7
Tamara Chernetska <cty2802@gmail.com>
2023-10-19 12:17:57 +0300

    removed clearState() from tests, added clearState() to createService

--- src/createServer.js
+++ src/createServer.js
@@ -3,6 +3,8 @@
 const cors = require('cors');
 const userRouter = require('./users/routes/user.route.js');
 const expnseRouter = require('./expenses/routes/expense.route.js');
+const userService = require('../src/users/services/user.service');
+const expenseService = require('../src/expenses/services/expense.service');
 
 const express = require('express');
 
@@ -10,6 +12,9 @@ function createServer() {
   const app = express();
 
   app.use(cors());
+
+  userService.clearState();
+  expenseService.clearState();
 
   app.use('/users', express.json(), userRouter);
   app.use('/expenses', express.json(), expnseRouter);
