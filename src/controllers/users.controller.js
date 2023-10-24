f3e2b20bb242aa3a222f437f6bcc98551e58dcd8
Klaudia <kklis@interia.pl>
2023-10-24 18:18:23 +0200

    Add task solution

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const userServices = require('../services/users.services');
+
+const getAllUsers = (_req, res) => {
+  res.send(userServices.getAllUsers());
+};
+
+const addUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  res.status(201);
+  res.send(userServices.createUser(name));
+};
+
+const getCurrentUser = (req, res) => {
+  const { userId } = req.params;
+  const findUser = userServices.getUserById(userId);
+
+  if (!findUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(findUser);
+};
+
+const removeUser = (req, res) => {
+  const { userId } = req.params;
+  const findUser = userServices.getUserById(userId);
+
+  if (!findUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  userServices.removeUser(userId);
+
+  res.sendStatus(204);
+};
+
+const updateUser = (req, res) => {
+  const { userId } = req.params;
+  const { name } = req.body;
+  const findUser = userServices.getUserById(userId);
+
+  if (!findUser) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  if (!name || typeof name !== 'string') {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  res.send(userServices.updateUser(userId, name));
+};
+
+module.exports = {
+  getAllUsers,
+  addUser,
+  getCurrentUser,
+  removeUser,
+  updateUser,
+};
