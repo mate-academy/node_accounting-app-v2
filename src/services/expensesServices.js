d9e47942e9fbbb90b874d1ea77693a043e7af624
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-10-25 10:17:30 +0200

    *

--- src/services/expensesServices.js
+++ src/services/expensesServices.js
@@ -10,18 +10,36 @@ const createExpense = (newExpense) => expenses.push(newExpense);
 
 const expenseIndex = (id) => expenses.findIndex(exp => exp.id === id);
 
-const updateExpense = (index, userId, spentAt, title, amount, category, note) =>
-  (
-    expenses[index] = {
-      ...expenses[index],
-      userId,
-      spentAt,
-      title,
-      amount,
-      category,
-      note,
-    }
-  );
+const updateExpense = (index,
+  userId, spentAt, title, amount, category, note) => {
+  const exp = expenses[index];
+
+  if (userId !== undefined) {
+    exp.userId = userId;
+  }
+
+  if (spentAt !== undefined) {
+    exp.spentAt = spentAt;
+  }
+
+  if (title !== undefined) {
+    exp.title = title;
+  }
+
+  if (amount !== undefined) {
+    exp.amount = amount;
+  }
+
+  if (category !== undefined) {
+    exp.category = category;
+  }
+
+  if (note !== undefined) {
+    exp.note = note;
+  }
+
+  return exp;
+};
 
 const deleteExpense = (index) => expenses.splice(index, 1);
 
