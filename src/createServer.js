5288fbf6da784c0671a75ca0b8bae3002dc3ef5e
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-12-05 13:43:08 +0200

    initial code

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,24 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const path = require('path');
+
+const users = [];
 
 function createServer() {
+  const app = express();
+
+  app.get('/users', express.json(), (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    res.send(users);
+  })
+
+
+
   // Use express to create a server
   // Add a routes to the server
-  // Return the server (express app)
+  return app;
 }
 
 module.exports = {
