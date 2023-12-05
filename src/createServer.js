a4d9cc6e7e22f7eafbe0eb4698b27ddb1e50712c
Vladyslav Atanov <gibepromt@gmail.com>
2023-12-05 23:26:18 +0200

    made accounting app v2

--- src/createServer.js
+++ src/createServer.js
@@ -6,12 +6,8 @@ const expenseRouter = require('./routes/expenseRoute').router;
 const cors = require('cors');
 const clearUsers = require('./services/users.service').clearUsers;
 const clearExpenses = require('./services/expenses.service').clearExpenses;
-// const expenseService = require('./services/expenses.service');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
   const app = express();
 
   clearUsers();
