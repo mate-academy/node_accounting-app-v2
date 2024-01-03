1f58bbfb6fcc84f140ab910c3391ab2100df7771
Yevhenii Tokmakov <matestudent0@gmail.com>
2024-01-03 19:22:32 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,12 +1,25 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+const { clearUsers } = require('./services/user.service');
+const { clearExpenses } = require('./services/expenses.service');
+const { userRouter } = require('./routes/user.route');
+const { expensesRouter } = require('./routes/expenses.route');
 
-function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
-}
+const app = express();
+
+const createServer = () => {
+  clearUsers();
+  clearExpenses();
+
+  app.use(cors(), express.json());
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expensesRouter);
+
+  return app;
+};
 
 module.exports = {
   createServer,
---
+++ src/services/user.service.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let users = [];
+
+const getAll = () => users;
+
+const getByUserId = (id) => users.find(u => u.id === +id) || null;
+
+const createUser = (name) => {
+  const newUser = {
+    id: users.length,
+    name,
+  };
+
+  users.push(newUser);
+
+  return newUser;
+};
+
+const deleteUser = (id) => users.filter(user => user.id !== +id);
+
+const updateUser = (user, name) => Object.assign(user, { name });
+
+const setAll = newUsers => {
+  users = newUsers;
+};
+
+const clearUsers = () => {
+  users = [];
+};
+
+module.exports = {
+  getAll,
+  getByUserId,
+  createUser,
+  deleteUser,
+  setAll,
+  clearUsers,
+  updateUser,
+};
