2cf4d802299956f68b72cdcdd771a82fab77e9cf
Sergey Emelyanov <mrleondono@gmail.com>
2024-01-18 20:24:37 +0200

    linter edits patch

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const expensesController = require('../controllers/expense.controller.js');
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const userController = require('../controllers/user.controller.js');
 
