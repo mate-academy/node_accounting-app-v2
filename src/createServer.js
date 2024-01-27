8f04c63618b36bbc66981a1075980c72f21ef53b
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2024-01-27 18:26:15 +0100

    Added empty methods

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,41 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const app = express();
 
 function createServer() {
   // Use express to create a server
   // Add a routes to the server
   // Return the server (express app)
+  app.get('/users', (req, res) => {
+  });
+
+  app.post('/users', (req, res) => {
+  });
+
+  app.get('/users/:id', (req, res) => {
+  });
+
+  app.delete('/users/:id', (req, res) => {
+  });
+
+  app.patch('/users:id', (req, res) => {
+  });
+
+  app.get('/expenses', (req, res) => {
+  });
+
+  app.post('/expenses', (req, res) => {
+  });
+
+  app.get('/expenses/:id', (req, res) => {
+  });
+
+  app.delete('/expenses/:id', (req, res) => {
+  });
+
+  app.patch('/expenses:id', (req, res) => {
+  });
 }
 
 module.exports = {
