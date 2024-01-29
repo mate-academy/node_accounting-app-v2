5e4a4d10b620b2ccb7079b04c2069bf70e9e9cbc
aurelia <aurelia.gorzna.dev@gmail.com>
2024-01-29 19:59:55 +0100

    instructions unclear. I had to guess what to do

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const { createServer } = require('./src/createServer');
+const express = require('express');
+const app = express();
 
-createServer()
-  .listen(3000, () => {
-    // eslint-disable-next-line no-console
-    console.log('Server is running on localhost:3000');
-  });
+app.use(express.json());
+
+const usersRouter = require('./src/routers/usersRouter');
+const expensesRouter = require('./src/routers/expensesRouter');
+
+app.use('/users', usersRouter);
+app.use('/expenses', expensesRouter);
+
+app.listen(process.env.PORT || 3000, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on localhost:${process.env.PORT || 3000}`);
+});
---
+++ src/routers/usersRouter.js
@@ -0,0 +1,82 @@
+/* eslint-disable max-len */
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+
+let users = [];
+
+let id = 0;
+
+router.get('/', (req, res) => {
+  res.send(users);
+});
+
+router.get('/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+
+  res.send(foundUser || 'User not found');
+});
+
+router.post('/', (req, res) => {
+  if (!req.body.name) {
+    res.status(400);
+    res.send();
+
+    return;
+  }
+
+  const newUser = {
+    id,
+    name: req.body.name,
+  };
+
+  users.push(newUser);
+
+  id++;
+
+  res.status(201);
+  res.send(newUser);
+});
+
+router.delete('/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+
+  if (!foundUser) {
+    res.status(404);
+    res.send();
+
+    return;
+  }
+
+  users = users.filter(user => user !== foundUser);
+
+  res.status(204);
+  res.send();
+});
+
+router.patch('/:id', (req, res) => {
+  const foundUser = users.findIndex(user => user.id === parseInt(req.params.id));
+
+  if (foundUser < 0) {
+    res.status(404);
+    res.send();
+
+    return;
+  }
+
+  if (!req.body.name) {
+    res.status(400);
+    res.send();
+
+    return;
+  }
+
+  users[foundUser].name = req.body.name;
+
+  res.status(200);
+  res.send();
+});
+
+module.exports = router;
