9c3237fcc9d91cb6b89041412cb1c20e0ac3ac6a
Laura Wronska <laura.wronskaa@gmail.com>
2024-01-30 01:35:54 +0100

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,33 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const { createUsers } = require('./collections/users');
+const { createExpenses } = require('./collections/expenses');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const userMethods = createUsers();
+  const expensesMethods = createExpenses();
+
+  const app = express();
+
+  app.get('/users', userMethods.getAll);
+  app.get('/users/:id', userMethods.getOne);
+  app.post('/users', express.json(), userMethods.post);
+  app.delete('/users/:id', userMethods.delete);
+  app.patch('/users/:id', express.json(), userMethods.patch);
+
+  app.get('/expenses', express.json(), expensesMethods.getAll);
+  app.get('/expenses/:id', expensesMethods.getOne);
+
+  app.post(
+    '/expenses',
+    express.json(),
+    expensesMethods.post(userMethods.users)
+  );
+  app.delete('/expenses/:id', expensesMethods.delete);
+  app.patch('/expenses/:id', express.json(), expensesMethods.patch);
+
+  return app;
 }
 
 module.exports = {
