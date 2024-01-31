17a2b3b0de985ac8cb307459df42226fcd0c86ce
Laura Wronska <laura.wronskaa@gmail.com>
2024-01-31 09:04:42 +0100

    add app.use(express.json))

--- src/createServer.js
+++ src/createServer.js
@@ -10,22 +10,19 @@ function createServer() {
 
   const app = express();
 
+  app.use(express.json());
+
   app.get('/users', userMethods.getAll);
   app.get('/users/:id', userMethods.getOne);
-  app.post('/users', express.json(), userMethods.post);
+  app.post('/users', userMethods.post);
   app.delete('/users/:id', userMethods.delete);
-  app.patch('/users/:id', express.json(), userMethods.patch);
+  app.patch('/users/:id', userMethods.patch);
 
-  app.get('/expenses', express.json(), expensesMethods.getAll);
+  app.get('/expenses', expensesMethods.getAll);
   app.get('/expenses/:id', expensesMethods.getOne);
-
-  app.post(
-    '/expenses',
-    express.json(),
-    expensesMethods.post(userMethods.users)
-  );
+  app.post('/expenses', expensesMethods.post(userMethods.users));
   app.delete('/expenses/:id', expensesMethods.delete);
-  app.patch('/expenses/:id', express.json(), expensesMethods.patch);
+  app.patch('/expenses/:id', expensesMethods.patch);
 
   return app;
 }
