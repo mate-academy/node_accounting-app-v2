50f9a00be4ce1688a59eab84f1015dbb04f40aae
Oleksii Zinchenko <chrispakulyak@gmail.com>
2024-03-10 22:58:13 +0200

    solution

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const userService = require('../services/user.service');
+
+const get = (req, res) => {
+  res.send(userService.getAllUsers());
+};
+
+const getById = (req, res) => {
+  const { id } = req.params;
+  const user = userService.getById(+id);
+
+  if (isNaN(id)) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  if (!user) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(user);
+};
+
+const create = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const user = userService.create(name);
+
+  res.statusCode = 201;
+  res.send(user);
+};
+
+const update = (req, res) => {
+  const id = +req.params.id;
+  const { name } = req.body;
+  const newUser = userService.update({
+    id, name,
+  });
+
+  res.send(newUser);
+};
+
+const remove = (req, res) => {
+  const id = +req.params.id;
+
+  if (!userService.getById(id)) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  userService.remove(id);
+
+  res.sendStatus(204);
+};
+
+module.exports = {
+  get,
+  getById,
+  create,
+  update,
+  remove,
+};
---
+++ src/services/expense.service.js
@@ -0,0 +1,77 @@
+'use strict';
+
+let expenses = [];
+
+const init = () => {
+  expenses = [];
+};
+
+const getAll = () => {
+  return expenses;
+};
+
+const getById = (id) => {
+  return expenses.find((item) => item.id === id) || null;
+};
+
+const create = (userId, spentAt, title, amount, category, note) => {
+  const newExpens = {
+    id: expenses.length,
+    userId,
+    spentAt,
+    title,
+    amount,
+    category,
+    note,
+  };
+
+  expenses.push(newExpens);
+
+  return newExpens;
+};
+
+const remove = (id) => {
+  expenses = expenses.filter((item) => item.id !== id);
+};
+
+const update = (id, args) => {
+  const expenseToUpdate = getById(id);
+
+  Object.assign(expenseToUpdate, { ...args });
+
+  return expenseToUpdate;
+};
+
+const filter = (id, categories, dateFrom, dateTo) => {
+  const filteredExpenses = (getAll()).filter((item) => {
+    if (!Number.isNaN(id) && item.userId !== id) {
+      return false;
+    }
+
+    if (categories && !categories.includes(item.category)) {
+      return false;
+    }
+
+    if (dateFrom && new Date(item.spentAt) < new Date(dateFrom)) {
+      return false;
+    }
+
+    if (dateTo && new Date(item.spentAt) > new Date(dateTo)) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return filteredExpenses;
+};
+
+module.exports = {
+  init,
+  getAll,
+  getById,
+  create,
+  remove,
+  update,
+  filter,
+};
