d198a93c2292b9dc39c5a9788d81ba0577338c7d
Oleksandr Boiko <iccc97@gmail.com>
2024-03-12 19:29:10 +0200

    add expenses

--- src/createServer.js
+++ src/createServer.js
@@ -2,14 +2,18 @@
 
 const express = require('express');
 const { usersRouter } = require('./routes/users.route');
+const { expensesRouter } = require('./routes/expenses.route');
 const usersService = require('./services/users.service');
+const expensesService = require('./services/expenses.service');
 
 function createServer() {
   const app = express();
 
   usersService.clearUsers();
+  expensesService.clearExpenses();
 
   app.use('/users', express.json(), usersRouter);
+  app.use('/expenses', express.json(), expensesRouter);
 
   return app;
 }
