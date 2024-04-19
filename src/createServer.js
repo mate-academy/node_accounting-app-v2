d4c36274ce213daf0eed601b9a884b3cc8ff7c24
Belia Anton <belja.anton@gmail.com>
2024-04-19 14:22:57 +0300

    add server user and expense api

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,21 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const { userRouter } = require('./routes/user.route');
+const { expenseRouter } = require('./routes/expense.route');
+const { userInit } = require('./services/user.service');
+const { expenseInit } = require('./services/expense.service');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  userInit();
+  expenseInit();
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  return app;
 }
 
 module.exports = {
