7c62a1177d80efb9bf1a1c5380f29e8129fa9384
Maryna Karpova <mari.artarea@gmail.com>
2024-06-14 17:28:45 +0200

    expenses pre

---
+++ src/controllers/expenses.controler.js
@@ -0,0 +1,10 @@
+// const expensesServise = require('../services/expenses.service');
+// const userServise = require('../services/user.service');
+
+// module.exports = {
+//   getAllExpense,
+//   createExpense,
+//   getOneExpense,
+//   deleteExpense,
+//   updateExpense,
+// };
