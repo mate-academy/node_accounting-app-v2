57812d25c414f93faa4dc3bf9b75ed6c1cc31e93
Maryna Karpova <mari.artarea@gmail.com>
2024-06-17 14:15:55 +0200

    changes

--- src/createServer.js
+++ src/createServer.js
@@ -3,7 +3,9 @@
 
 const express = require('express');
 const { userRouter } = require('./routers/user.router');
-// const { expensesRouter } = require('./routers/expenses.router');
+const { expensesRouter } = require('./routers/expenses.router');
+const expenseServices = require('./services/expenses.service');
+const usersServices = require('./services/user.service');
 
 function createServer() {
   const app = express();
@@ -14,8 +16,11 @@ function createServer() {
     response.send('Hello');
   });
 
-  app.use('/users', userRouter);
-  // app.use('/router', expensesRouter);
+  expenseServices.reset();
+  usersServices.reset();
+
+  app.use('/users', express.json(), userRouter);
+  app.use('/expenses', express.json(), expensesRouter);
 
   return app;
 }
--- src/routers/expenses.router.js
+++ src/routers/expenses.router.js
@@ -1,14 +1,15 @@
-// const express = require('express');
-// const expensesController = require('../controllers/expenses.controler');
+/* eslint-disable no-undef */
+const express = require('express');
+const expensesController = require('../controllers/expenses.controler');
 
-// const expensesRouter = express.Router();
+const expensesRouter = express.Router();
 
-// expensesRouter.get('/', expensesController.getAllExpense);
-// expensesRouter.post('/', expensesController.createExpense);
-// expensesRouter.get('/:id', expensesController.getOneExpense);
-// expensesRouter.delete('/:id', expensesController.deleteExpense);
-// expensesRouter.patch('/:id', expensesController.updateExpense);
+expensesRouter.get('/', expensesController.getAllExpense);
+expensesRouter.post('/', expensesController.createExpense);
+expensesRouter.get('/:id', expensesController.getOneExpense);
+expensesRouter.delete('/:id', expensesController.deleteExpense);
+expensesRouter.patch('/:id', expensesController.updateExpense);
 
-// module.exports = {
-//   expensesRouter,
-// };
+module.exports = {
+  expensesRouter,
+};
