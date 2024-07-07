4be664ded399ec674ced3dfdb6997a105c0937e3
mfcastilho <fredericocastilho@hotmail.com>
2024-07-07 18:18:29 -0300

    feat: create GetUsersService file

---
+++ src/services/user/GetUsersService.js
@@ -0,0 +1,11 @@
+const userRepository = require('../../repositories/userRepository');
+
+class GetUserService {
+	execute() {
+		const users = userRepository.findAll();
+
+		return users;
+	}
+}
+
+module.exports = GetUserService;
---
+++ src/services/user/index.js
@@ -0,0 +1,5 @@
+const GetUserService = require('./GetUsersService');
+
+module.exports = {
+  GetUserService,
+}
