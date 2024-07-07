45eee0f3dec8ed18295bd2e05307853e183cf274
mfcastilho <fredericocastilho@hotmail.com>
2024-07-07 20:49:51 -0300

    feat: create EditUserController file

---
+++ src/controllers/user/EditUserController.js
@@ -0,0 +1,19 @@
+const { EditUserService } = require('../../services/user');
+
+class EditUserController {
+  async handle(req, res) {
+    const {id} = req.params;
+
+    try {
+      const service = new EditUserService();
+
+      const user = service.execute(id, req.body);
+
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server error' });
+    }
+  }
+}
+
+module.exports = new EditUserController();
