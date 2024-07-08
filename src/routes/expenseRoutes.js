e646d4ae1ef4c563305d4211e6a9370ff9e2a64b
mfcastilho <fredericocastilho@hotmail.com>
2024-07-08 02:29:53 -0300

    feat: create EditExpenseController and EditExpenseService

--- src/routes/expenseRoutes.js
+++ src/routes/expenseRoutes.js
@@ -3,15 +3,33 @@ const {
   GetAllExpensesController,
   CreateExpenseController,
   GetExpenseController,
+  EditExpenseController,
 } = require('../controllers/expense');
-const { verifyExpenseFields, verifyIfExpenseExists, verifyIfUserExists, verifyIfIdIsANumber } = require('../middlewares');
+const {
+  verifyExpenseFields,
+  verifyIfExpenseExists,
+  verifyIfUserExists,
+  verifyIfIdIsANumber,
+} = require('../middlewares');
 
 const expenseRoutes = Router();
 
 expenseRoutes.get('/', GetAllExpensesController.handle);
-expenseRoutes.get('/:id', verifyIfIdIsANumber, verifyIfExpenseExists, GetExpenseController.handle);
-expenseRoutes.post('/', verifyIfUserExists, verifyExpenseFields, CreateExpenseController.handle);
-expenseRoutes.patch('/:id');
+
+expenseRoutes.get(
+  '/:id',
+  verifyIfIdIsANumber,
+  verifyIfExpenseExists,
+  GetExpenseController.handle,
+);
+
+expenseRoutes.post(
+  '/',
+  verifyIfUserExists,
+  verifyExpenseFields,
+  CreateExpenseController.handle,
+);
+expenseRoutes.patch('/:id',  verifyIfIdIsANumber, verifyIfExpenseExists, verifyIfUserExists, verifyExpenseFields, EditExpenseController.handle);
 expenseRoutes.delete('/:id');
 
 module.exports = expenseRoutes;
