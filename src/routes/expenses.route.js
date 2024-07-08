0b2d6d98ea690373c59138663878decc0d15a726
Grygoryy <kachurovskyy123@gmail.com>
2024-07-08 14:06:48 +0300

    solution

---
+++ src/routes/expenses.route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const expenseController = require('../controllers/expenses.controller');
+
+const router = express.Router();
+
+router.post('/', expenseController.createExpense);
+router.get('/', expenseController.getExpenses);
+router.get('/:id', expenseController.getExpenseById);
+router.patch('/:id', expenseController.updateExpense);
+router.delete('/:id', expenseController.deleteExpense);
+
+module.exports = router;
