e7c84a255d0bd59468b48525097472f4bff2e995
mfcastilho <fredericocastilho@hotmail.com>
2024-07-08 02:18:05 -0300

    feat: create verifyExpenseField middleware

---
+++ src/middlewares/verifyIfExpenseExists.js
@@ -0,0 +1,17 @@
+const expenseRepository = require('../repositories/expenseRepository');
+
+const verifyIfExpenseExists = (req, res, next) => {
+  const { id } = req.params;
+
+  const expenseExists = expenseRepository
+    .findAll()
+    .find(expense => expense.id === Number(id));
+
+    if (!expenseExists) {
+      return res.status(404).json({ error: 'Expense not found' });
+    }
+  
+    return next();
+}
+
+module.exports = verifyIfExpenseExists;
