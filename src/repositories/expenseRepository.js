d7de878105269cfea2dfcbacb9e699f60cd443af
mfcastilho <fredericocastilho@hotmail.com>
2024-07-07 23:46:45 -0300

    feat: create expenseRepository file

---
+++ src/repositories/expenseRepository.js
@@ -0,0 +1,88 @@
+const mockExpenses = require('../api/mockExpenses');
+
+const expenseRepository = {
+  create: (expenseData) => {
+    const newExpense = {
+      id: mockExpenses.length + 1,
+      ...expenseData,
+    };
+
+    mockExpenses.push(newExpense);
+
+    return newExpense;
+  },
+
+  findAll: (filters) => {
+    let expenses = mockExpenses;
+
+    const { userId, from, to, categories } = filters;
+
+    if (userId) {
+      expenses = expenses.filter(
+        (expense) => expense.userId === Number(userId),
+      );
+    }
+
+    if (from) {
+      const fromDate = new Date(from);
+
+      expenses = expenses.filter(
+        (expense) => new Date(expense.spentAt) >= fromDate,
+      );
+    }
+
+    if (to) {
+      const toDate = new Date(to);
+
+      expenses = expenses.filter(
+        (expense) => new Date(expense.spentAt) <= toDate,
+      );
+    }
+
+    if (categories && categories.length > 0) {
+      expenses = expenses.filter((expense) =>
+        // eslint-disable-next-line function-paren-newline
+        categories.includes(expense.category)
+      );
+    }
+
+    return expenses;
+  },
+
+  findByPk: (expenseId) => {
+    return mockExpenses.find((expense) => expense.id === Number(expenseId));
+  },
+
+  findOne: (field, value) => {
+    return mockExpenses.find((expense) => expense[field] === value);
+  },
+
+  update: (expenseId, expenseData) => {
+    const { title, amount, category, note } = expenseData;
+
+    const updatedExpense = mockExpenses.find(
+      (expense) => expense.id === Number(expenseId),
+    );
+
+    updatedExpense.title = title ?? updatedExpense.title;
+    updatedExpense.amount = amount ?? updatedExpense.amount;
+    updatedExpense.category = category ?? updatedExpense.category;
+    updatedExpense.note = note ?? updatedExpense.note;
+
+    return updatedExpense;
+  },
+
+  destroy: (expenseId) => {
+    const expenseIndex = mockExpenses.findIndex(
+      (expense) => expense.id === Number(expenseId),
+    );
+
+    return mockExpenses.splice(expenseIndex, 1)[0];
+  },
+
+  resetExpenses: () => {
+    mockExpenses.length = 0;
+  },
+};
+
+module.exports = expenseRepository;
