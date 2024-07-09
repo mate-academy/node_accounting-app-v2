3a31a15136cb2a50fd3c5609763762feb56257b2
Oleksii Dunchyk <oleksiidunchik@gmail.com>
2024-07-09 14:03:20 +0300

    node accounting app

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,82 @@
+const userService = require('../services/user.service');
+
+const getAllUsers = (req, res) => {
+  const users = userService.getAllUsers();
+
+  res.send(users);
+};
+
+const createUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const newUser = userService.createUser(name);
+
+  return res.status(201).json(newUser);
+};
+
+const getUser = (req, res) => {
+  const { id } = req.params;
+
+  const user = userService.getUserById(id);
+
+  if (!user) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  res.send(user);
+};
+
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  const user = userService.getUserById(id);
+
+  if (!user) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  userService.deleteUser(id);
+
+  res.sendStatus(204);
+};
+
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const user = userService.getUserById(id);
+
+  if (!user) {
+    res.sendStatus(404);
+
+    return;
+  }
+
+  if (!name) {
+    res.sendStatus(400);
+
+    return;
+  }
+
+  const updatedUser = userService.updateUser(id, name);
+
+  res.send(updatedUser);
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUser,
+  deleteUser,
+  updateUser,
+};
