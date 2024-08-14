1cbdb050dc1847e128c7fc2eee86a40cd2ac79e5
IhorVerba <ihor.verbivskyi@gmail.com>
2024-08-14 15:53:35 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,29 @@
 'use strict';
 
-// const express = require('express');
+/* eslint-disable no-console */
+const express = require('express');
+const userService = require('./services/user.service.js');
+const expenseService = require('./services/expense.service.js');
+const userRouter = require('./routes/user.routes.js');
+const expenseRouter = require('./routes/expense.routes.js');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(express.json());
+
+  userService.init();
+  expenseService.init();
+
+  app.use('/users', userRouter);
+  app.use('/expenses', expenseRouter);
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  });
+
+  return app;
 }
 
 module.exports = {
