3af2de90f4bd8e58965c1aca31ff43e1e7e85724
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2025-01-04 17:39:30 -0600

    Solution

---
+++ src/services/expenses.service.js
@@ -0,0 +1,88 @@
+const { generateId } = require('../utils/generateId');
+const expenses = [];
+
+function clearExpenses() {
+  expenses.length = 0;
+}
+
+function getAll(query) {
+  const { userId, categories, from, to } = query;
+
+  return expenses.filter((expense) => {
+    if (userId && expense.userId !== +userId) {
+      return false;
+    }
+
+    if (
+      categories &&
+      Array.isArray(categories) &&
+      !categories.includes(expense.category)
+    ) {
+      return false;
+    }
+
+    if (
+      categories &&
+      !Array.isArray(categories) &&
+      categories !== expense.category
+    ) {
+      return false;
+    }
+
+    if (from && from > expense.spentAt) {
+      return false;
+    }
+
+    if (to && to < expense.spentAt) {
+      return false;
+    }
+
+    return true;
+  });
+}
+
+function getById(id) {
+  return expenses.find((expense) => expense.id === id);
+}
+
+function create(params) {
+  const expense = {
+    id: generateId(),
+    ...params,
+  };
+
+  expenses.push(expense);
+
+  return expense;
+}
+
+function deleteById(id) {
+  const index = expenses.findIndex((expense) => expense.id === id);
+
+  if (index === -1) {
+    return;
+  }
+
+  const [deletedExpense] = expenses.splice(index, 1);
+
+  return deletedExpense;
+}
+
+function update(id, params) {
+  const expensetoUpdate = expenses.find((expense) => expense.id === id);
+
+  if (!expensetoUpdate) {
+    return;
+  }
+
+  return Object.assign(expensetoUpdate, params);
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  deleteById,
+  update,
+  clearExpenses,
+};
---
+++ src/services/users.service.js
@@ -0,0 +1,54 @@
+const { generateId } = require('../utils/generateId');
+
+const users = [];
+
+function clearUsers() {
+  users.length = 0;
+}
+
+function getAll() {
+  return users;
+}
+
+function getById(id) {
+  return users.find((user) => user.id === id);
+}
+
+function create(name) {
+  const user = { id: generateId(), name };
+
+  users.push(user);
+
+  return user;
+}
+
+function deleteById(id) {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index === -1) {
+    return;
+  }
+
+  const [deletedUser] = users.splice(index, 1);
+
+  return deletedUser;
+}
+
+function update(id, name) {
+  const usertoUpdate = users.find((user) => user.id === id);
+
+  if (!usertoUpdate) {
+    return;
+  }
+
+  return Object.assign(usertoUpdate, { name });
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  deleteById,
+  update,
+  clearUsers,
+};
