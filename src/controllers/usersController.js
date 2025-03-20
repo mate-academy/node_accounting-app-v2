fa478754ca8db79f255b6365ea275ff075b6e155
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-03-20 15:38:11 +0200

    Implemented an expense tracking app

---
+++ src/controllers/usersController.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const usersService = require('../services/usersService');
+
+const getAllUsers = (req, res) => {
+  const users = usersService.getAll();
+
+  res.status(200).send(users);
+};
+
+const getOneUser = (req, res) => {
+  const { id } = req.params;
+  const user = usersService.getById(+id);
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  res.status(200).send(user);
+};
+
+const createUser = (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
+  const user = usersService.create(name);
+
+  res.status(201).send(user);
+};
+
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const user = usersService.getById(+id);
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  const updatedUser = usersService.update(+id, name);
+
+  res.status(200).send(updatedUser);
+};
+
+const removeUser = (req, res) => {
+  const { id } = req.params;
+  const user = usersService.getById(+id);
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  usersService.remove(+id);
+  res.sendStatus(204);
+};
+
+module.exports = {
+  getAllUsers,
+  getOneUser,
+  createUser,
+  updateUser,
+  removeUser,
+};
--- src/createServer.js
+++ src/createServer.js
@@ -1,13 +1,26 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+
+const usersRouter = require('./routes/usersRoutes');
+const expensesRouter = require('./routes/expensesRoutes');
+const usersService = require('./services/usersService');
+const expensesService = require('./services/expensesService');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  usersService.clearAll();
+  expensesService.clearAll();
+
+  app.use(cors());
+  app.use(express.json());
+
+  app.use('/users', usersRouter);
+  app.use('/expenses', expensesRouter);
+
+  return app;
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
