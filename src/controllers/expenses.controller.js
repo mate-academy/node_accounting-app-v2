392b9348428f29283a4893f4e53ea1045ecfba4d
Bohdan Ivankov <bodanich600@gmail.com>
2025-04-01 12:23:43 +0200

    test

---
+++ src/controllers/expenses.controller.js
@@ -0,0 +1,97 @@
+const { expensesService } = require('../services/expenses.service');
+const { userService } = require('../services/users.service');
+
+const expensesController = {
+  getAll: (req, res) => {
+    const { userId, categories, from, to } = req.query;
+
+    const expenses = expensesService.getAll({
+      userId,
+      categories,
+      from,
+      to,
+    });
+
+    res.send(expenses);
+  },
+
+  getOne: (req, res) => {
+    const expense = expensesService.getExpenseById(+req.params.id);
+
+    if (!expense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    res.send(expense);
+  },
+
+  create: (req, res) => {
+    const { userId, spentAt, title, amount, category, note } = req.body;
+
+    if (!userId || !spentAt || !title || !amount || !category || !note) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const user = userService.getUserById(userId);
+
+    if (!user) {
+      res.sendStatus(400);
+
+      return;
+    }
+
+    const newExpense = expensesService.create({
+      userId,
+      spentAt,
+      title,
+      amount,
+      category,
+      note,
+    });
+
+    res.status(201).send(newExpense);
+  },
+
+  remove: (req, res) => {
+    const expense = expensesService.getExpenseById(+req.params.id);
+
+    if (!expense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    expensesService.remove(+req.params.id);
+    res.sendStatus(204);
+  },
+
+  update: (req, res) => {
+    const id = +req.params.id;
+    const { userId, spentAt, title, amount, category, note } = req.body;
+
+    const expense = expensesService.update(id, {
+      userId,
+      spentAt,
+      title,
+      amount,
+      category,
+      note,
+    });
+
+    if (!expense) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    res.send(expense);
+  },
+};
+
+module.exports = {
+  expensesController,
+};
