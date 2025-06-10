ccf81b8e03306554203ae593a4cb6043fa577225
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-06-10 10:05:49 +0100

    Node accounting app v2 solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,25 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const cors = require('cors');
+const { usersRouter } = require('./routes/usersRoutes');
+const { expensesRouter } = require('./routes/expensesRoutes');
+const { resetExpenses } = require('./controllers/expensesController');
+const { resetUsers } = require('./controllers/usersController');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+
+  resetUsers();
+  resetExpenses();
+
+  app.use('/users', usersRouter);
+  app.use('/expenses', expensesRouter);
+
+  return app;
 }
 
 module.exports = {
