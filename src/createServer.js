849633c8fb5af055f7b3857f0d34bd905f969acc
Ihor Barban <barbanihor@gmail.com>
2025-07-11 14:29:04 +0200

    Task done

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,23 @@
 'use strict';
 
-// const express = require('express');
+const userRouter = require('./users/users.routes');
+const expensesRouter = require('./expenses/expenses.routes');
+const usersData = require('./resources/usersData');
+const expensesData = require('./resources/expenseData');
+const express = require('express');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  usersData.resetUsers();
+  expensesData.resetExpenses();
+
+  app.use(express.json());
+
+  app.use('/users', express.json(), userRouter);
+  app.use('/expenses', express.json(), expensesRouter);
+
+  return app;
 }
 
 module.exports = {
