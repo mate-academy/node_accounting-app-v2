36c49869c4c22ebff91ba4d27ad20c4802b46501
Arber <1234arberzylyftari1234@gmail.com>
2025-09-02 15:30:41 +0200

    Node accounting app solution

---
+++ src/routes/users.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+module.exports = function (users) {
+  const router = express.Router();
+
+  let userIdCounter = 1;
+
+  router.post('/', (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const newUser = { id: userIdCounter++, name };
+
+    users.push(newUser);
+    res.status(201).json(newUser);
+  });
+
+  router.get('/', (req, res) => {
+    res.json(users);
+  });
+
+  router.get('/:id', (req, res) => {
+    const user = users.find((u) => u.id === parseInt(req.params.id));
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    res.json(user);
+  });
+
+  router.patch('/:id', (req, res) => {
+    const user = users.find((u) => u.id === parseInt(req.params.id));
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const { name } = req.body;
+
+    if (name) {
+      user.name = name;
+    }
+
+    res.json(user);
+  });
+
+  router.delete('/:id', (req, res) => {
+    const index = users.findIndex((u) => u.id === parseInt(req.params.id));
+
+    if (index === -1) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    users.splice(index, 1);
+    res.status(204).end();
+  });
+
+  return router;
+};
