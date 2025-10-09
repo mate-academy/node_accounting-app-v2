5cdf85631cb86d435be5909663d363a2a011ba83
Viktoriia <viktoriamyhailiak@gmail.com>
2025-10-09 17:44:05 +0300

    add a solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,13 +1,163 @@
-'use strict';
-
-// const express = require('express');
+const express = require('express');
 
 function createServer() {
-  // Use express to create a server
-  // Add a routes to the server
-  // Return the server (express app)
+  const app = express();
+
+  app.use(express.json());
+
+  let users = [];
+  let expenses = [];
+  let userIdCount = 1;
+  let expenseId = 1;
+
+  app.get('/users', (req, res) => {
+    return res.json(users);
+  });
+
+  app.get('/expenses', (req, res) => {
+    const { userId, categories, from, to } = req.query;
+    let filtered = [...expenses];
+
+    if (userId) {
+      filtered = filtered.filter((x) => x.userId === +userId);
+    }
+
+    if (categories) {
+      filtered = filtered.filter((x) => x.category === categories);
+    }
+
+    const fromDate = new Date(from);
+    const toDate = new Date(to);
+
+    if (from) {
+      filtered = filtered.filter((e) => fromDate <= new Date(e.spentAt));
+    }
+
+    if (to) {
+      filtered = filtered.filter((e) => new Date(e.spentAt) <= toDate);
+    }
+
+    return res.status(200).json(filtered);
+  });
+
+  app.post('/users', (req, res) => {
+    const name = req.body.name;
+
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required' });
+    }
+
+    const newUser = {
+      id: userIdCount,
+      name,
+    };
+
+    users.push(newUser);
+    userIdCount++;
+
+    res.status(201).json(newUser);
+  });
+
+  app.post('/expenses', (req, res) => {
+    const { userId, spentAt, title, amount, category, note } = req.body;
+
+    if (!userId || !spentAt || !title || !amount || !category || !note) {
+      return res.status(400).json({ message: 'Bad request' });
+    }
+
+    const user = users.find((x) => x.id === +userId);
+
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    const newExpense = {
+      id: expenseId,
+      userId,
+      spentAt,
+      title,
+      amount: +amount,
+      category,
+      note,
+    };
+
+    expenses.push(newExpense);
+    expenseId++;
+
+    res.status(201).json(newExpense);
+  });
+
+  app.get('/users/:id', (req, res) => {
+    const user = users.find((u) => u.id === +req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  });
+
+  app.get('/expenses/:id', (req, res) => {
+    const expense = expenses.find((e) => e.id === +req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({ message: 'Expense not found' });
+    }
+
+    res.status(200).json(expense);
+  });
+
+  app.delete('/users/:id', (req, res) => {
+    const user = users.find((u) => u.id === +req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    users = users.filter((u) => u.id !== +req.params.id);
+
+    res.status(204).end();
+  });
+
+  app.delete('/expenses/:id', (req, res) => {
+    const expense = expenses.find((e) => e.id === +req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({ message: 'Expense not found' });
+    }
+
+    expenses = expenses.filter((u) => u.id !== +req.params.id);
+    res.status(204).end();
+  });
+
+  app.patch('/users/:id', (req, res) => {
+    const user = users.find((u) => u.id === +req.params.id);
+    const name = req.body.name;
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (!name) {
+      return res.status(400).json({ message: 'Name is required' });
+    }
+
+    user.name = name;
+    res.status(200).json(user);
+  });
+
+  app.patch('/expenses/:id', (req, res) => {
+    const expense = expenses.find((e) => e.id === +req.params.id);
+
+    if (!expense) {
+      return res.status(404).json({ message: 'Expense not found' });
+    }
+
+    Object.assign(expense, req.body, { id: expense.id });
+    res.status(200).json(expense);
+  });
+
+  return app;
 }
 
-module.exports = {
-  createServer,
-};
+module.exports = { createServer };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const { createServer } = require('./createServer');
+import { createServer } from './createServer';
 
-createServer().listen(3000, () => {
+const app = createServer();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server is running on localhost:3000');
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
