e67dda980b7177ce67dedf01ec7b8719d932e66c
Andrii Tsopych <andriythoupich01@gmail.com>
2026-04-05 14:40:27 +0300

    add task implementation

--- src/createServer.js
+++ src/createServer.js
@@ -1,11 +1,24 @@
 'use strict';
 
-// const express = require('express');
+const express = require('express');
+const createExpensesRouter = require('./routes/expenses');
+const createUsersRouter = require('./routes/users');
 
 function createServer() {
+  const app = express();
+
   // Use express to create a server
   // Add a routes to the server
   // Return the server (express app)
+  app.use(express.json());
+
+  const expenses = [];
+  const users = [];
+
+  app.use('/expenses', createExpensesRouter(expenses, users));
+  app.use('/users', createUsersRouter(users));
+
+  return app;
 }
 
 module.exports = {
